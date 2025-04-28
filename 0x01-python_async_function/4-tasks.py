077be0cd084adfc9b1c759392c545a69287c14c5
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-04-28 20:37:25 +0100

    Task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,6 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Alternate function to the older wait_n function
