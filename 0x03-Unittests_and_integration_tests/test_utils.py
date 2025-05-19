05d677b0bf11bb6a43587201114c15195713e39a
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-19 13:31:32 +0100

    0-task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,6 +13,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {'b': 2})
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Testing method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
