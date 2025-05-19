0efa0fc745a476d6007fce457c1068cd5723ec10
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-19 14:37:27 +0100

    1-try2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,7 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
-        """Test for exceptions"""
+        """Test for exceptions in the method"""
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
-        self.assertEqual(f"KeyError('{expected})", repr(e.exception))
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
